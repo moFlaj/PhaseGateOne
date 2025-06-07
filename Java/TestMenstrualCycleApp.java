963d4947a9c3ee965dfcdadc3fd38697142b5e2d
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-07 19:46:33 +0100

    Update TestMenstrualCycleApp.java
    Also modified my test cases to take two arguments. Values for numberOfFlowDays and nextPeriodStartDate can now be changed. Some women have shorter flow days

--- Java/TestMenstrualCycleApp.java
+++ Java/TestMenstrualCycleApp.java
@@ -10,16 +10,20 @@ public class TestMenstrualCycleAppMod{
 	String inputDate;
 	String actual;
 	String expected;
+	int numberOfFlowDays;
+	int cycleLength;
 
 	@BeforeEach
 	void setUp(){
 		inputDate = "2025-06-29";
-		
+		numberOfFlowDays = 5;
+		cycleLength = 28;
+			
 	}
 
 	@Test
 	void testPeriodFlowDaysReturnsCorrectDateIfFlowSpansFromCurrentMonthToTheNextMonthOfSameYear(){
-		actual = MenstrualCycleAppMod.periodFlowDays(inputDate);
+		actual = MenstrualCycleAppMod.periodFlowDays(inputDate, numberOfFlowDays);
 		expected = "Your period will likely last from Sunday, 2025-06-29 to Friday, 2025-07-04.";
 		assertEquals(expected, actual);
 	}
@@ -27,7 +31,7 @@ public class TestMenstrualCycleAppMod{
 	@Test
 	void testPeriodFlowDaysReturnsCorrectDateIfFlowSpansFromCurrentMonthToTheNextMonthOfDifferentYear(){
 		inputDate = "2025-12-29";
-		actual = MenstrualCycleAppMod.periodFlowDays(inputDate);
+		actual = MenstrualCycleAppMod.periodFlowDays(inputDate, numberOfFlowDays);
 		expected = "Your period will likely last from Monday, 2025-12-29 to Saturday, 2026-01-03.";
 		assertEquals(expected, actual);
 	}
@@ -37,7 +41,7 @@ public class TestMenstrualCycleAppMod{
 // Month 13 when there are only 12 months in a year.
 		inputDate = "2025-13-29";
 		assertThrows(IllegalArgumentException.class, () -> {
-			MenstrualCycleAppMod.periodFlowDays(inputDate);
+			MenstrualCycleAppMod.periodFlowDays(inputDate, numberOfFlowDays);
 		});
 	}
 
@@ -46,8 +50,25 @@ public class TestMenstrualCycleAppMod{
 // Format specified is YYYY-MM-DD.
 		inputDate = "2025-29-12";
 		assertThrows(IllegalArgumentException.class, () -> {
-			MenstrualCycleAppMod.periodFlowDays(inputDate);
+			MenstrualCycleAppMod.periodFlowDays(inputDate, numberOfFlowDays);
 		});
 	}
 
+	@Test
+	void testNextPeriodStartDateReturnsNextPeriodStartDateWhichIs28DaysAfterStartDateOfCurrentOrPreviousPeriod(){
+		actual = MenstrualCycleAppMod.nextPeriodStartDate(inputDate, cycleLength);
+		expected = "Your next period begins on Sunday, 2025-07-27.";
+		assertEquals(expected, actual);
+
+	}
+
+
+	@Test
+	void testNextPeriodStartDateThrowsErrorIfIncorrectDateInputIsEntered(){
+		// Format specified is YYYY-MM-DD.
+		inputDate = "2025-29-12";
+		assertThrows(IllegalArgumentException.class, () -> {
+			MenstrualCycleAppMod.nextPeriodStartDate(inputDate, cycleLength);
+		});
+	}
 }
