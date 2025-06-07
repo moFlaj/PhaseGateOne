87e306ce89a7fa2b8f2f9df6ce13da02c9e7f77d
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-07 17:59:52 +0100

    Update TestMenstrualCycleApp.java
    Updated my tests for the new approach I took for this task

--- Java/TestMenstrualCycleApp.java
+++ Java/TestMenstrualCycleApp.java
@@ -1,141 +1,53 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import java.util.InputMismatchException;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class TestMenstrualCycleApp{
-	String month;
-	String day;
-	List<String> months;
+public class TestMenstrualCycleAppMod{
+	String inputDate;
 	String actual;
 	String expected;
 
 	@BeforeEach
 	void setUp(){
-		month = "February";
-		day = "27";
-		months = List.of("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-
+		inputDate = "2025-06-29";
+		
 	}
 
 	@Test
-	void testPeriodFlowDatesExists(){
-		MenstrualCycleApp.periodFlowDates(month,day,months);
-
-	}
-	
-	@Test
-	void testPeriodFlowOnlyAcceptsWordsAsItsFirstArgument(){
-		month = "5";
-		assertThrows(InputMismatchException.class, () -> {
-			MenstrualCycleApp.periodFlowDates(month,day,months);
-		});
+	void testPeriodFlowDaysReturnsCorrectDateIfFlowSpansFromCurrentMonthToTheNextMonthOfSameYear(){
+		actual = MenstrualCycleAppMod.periodFlowDays(inputDate);
+		expected = "Your period will likely last from Sunday, 2025-06-29 to Friday, 2025-07-04.";
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	void testPeriodFlowDateThrowsErrorIfMonthEnteredIsNotValid(){
-		month = "Blue";
+	void testPeriodFlowDaysReturnsCorrectDateIfFlowSpansFromCurrentMonthToTheNextMonthOfDifferentYear(){
+		inputDate = "2025-12-29";
+		actual = MenstrualCycleAppMod.periodFlowDays(inputDate);
+		expected = "Your period will likely last from Monday, 2025-12-29 to Saturday, 2026-01-03.";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testPeriodFlowDaysThrowsErrorIfInvalidDateInputIsEntered(){
+// Month 13 when there are only 12 months in a year.
+		inputDate = "2025-13-29";
 		assertThrows(IllegalArgumentException.class, () -> {
-			MenstrualCycleApp.periodFlowDates(month,day,months);
+			MenstrualCycleAppMod.periodFlowDays(inputDate);
 		});
 	}
 
 	@Test
-	void testRuntimeErrorOccursIfAnythingAsidesAnIntegerIsPassedAsSecondArgument(){
-		try{
-			day = "REYYD";
-			MenstrualCycleApp.periodFlowDates(month,day,months);
-		}catch(NumberFormatException error){
-			actual = error.getMessage();
-		}
-		expected = "For input string: \"REYYD\"";
-		assertEquals(expected, actual);
-	}
-	@Test
-	void testPeriodFlowDatesThrowsErrorIfANegativeDayInputIsEntered(){
-		day = "-30";
+	void testPeriodFlowDaysThrowsErrorIfDateIsInputIsNotOfFormatSpecifiedInLogic(){
+// Format specified is YYYY-MM-DD.
+		inputDate = "2025-29-12";
 		assertThrows(IllegalArgumentException.class, () -> {
-			MenstrualCycleApp.periodFlowDates(month,day,months);
+			MenstrualCycleAppMod.periodFlowDays(inputDate);
 		});
 	}
-
-	@Test
-	void testPerioFlowDatesThrowsErrorIfDayEnteredExceedsNumberOfDaysInChosenMonth(){
-		day = "30";
-		assertThrows(IllegalArgumentException.class, () -> {
-			MenstrualCycleApp.periodFlowDates(month,day,months);
-		});
-	}
-
-	@Test
-	void testPeriodFlowDateShowsHowLongFlowDaysMayLastIfRequiredInformationIsProvided(){
-		actual = MenstrualCycleApp.periodFlowDates(month,day,months);
-		expected =  "Your period will likely last from February 27 to March 3";
-		assertEquals(expected, actual);
-
-	}
-
-	@Test
-	void testNextPeriodStartDateReturnsCorrectDateIfNextPeriodWillBeginInTheUpcomingMonth(){
-		actual = MenstrualCycleApp.nextPeriodStartDate(month,day,months);
-		expected = "Your next period begins on March 27";
-		assertEquals(expected, actual);
-
-	}
-
-	@Test
-	void testNextPeriodStartDateReturnsCorrectDateIfNextPeriodWillBeginInTheSameMonthAsPreviousAndMonthIsNotFebruary(){
-		month = "June";
-		day = "1";
-		actual = MenstrualCycleApp.nextPeriodStartDate(month,day,months);
-		expected = "Your next period begins on June 29";
-		assertEquals(expected, actual);
-
-	}
-
-	@Test
-	void testFindOvulationDayOccurs14daysBeforeNextPeriodStartDate(){
-		month = "June";
-		day = "1";
-		actual = MenstrualCycleApp.nextPeriodStartDate(month,day,months) + ". "  + MenstrualCycleApp.findOvulationDay(month, day, months) + ".";
-		expected =  "Your next period begins on June 29. Your ovulation day is June 15.";
-		assertEquals(expected, actual);
-
-	}
-
-	@Test
-	void testDurationOfFertileWindowToConfirmIfItLastsFromFiveDaysBeforeOvulationDayAndOneDayAfterOvulationDay(){
-		month = "June";
-		day = "1";
-		actual = MenstrualCycleApp.findOvulationDay(month, day, months) + ". " + MenstrualCycleApp.identifyFertileWindow(month,day,months) + ".";
-		expected = "Your ovulation day is June 15. Fertile window will likely start on June 10, to end on June 16.";
-		assertEquals(expected, actual);
-
-	}
-
-	@Test
-	void testDurationOfFertileWindowIfOvulationDayDoesNotOccurInTheSameMonthAsFirstPeriodFlowDate(){
-		month = "June";
-		day = "30";
-		actual = MenstrualCycleApp.findOvulationDay(month, day, months) + ". " + MenstrualCycleApp.identifyFertileWindow(month,day,months) + ".";
-		expected = "Your ovulation day is July 14. Fertile window will likely start on July 9, to end on July 15.";
-		assertEquals(expected, actual);
-
-	}
-
-	@Test
-	void testMarkSafePeriodReturnsCorrectSafePeriodDuration(){
-		actual = MenstrualCycleApp.findOvulationDay(month, day, months) + ". " + MenstrualCycleApp.identifyFertileWindow(month,day,months) + ". " + MenstrualCycleApp.markSafePeriods(month,day,months) + ".";
-		expected = "Your ovulation day is March 13. Fertile window will likely start on March 8, to end on March 14. Your safe period is from February 27 to March 7 and March 15 to March 26.";
-		assertEquals(expected, actual);
-
-	}
-
 
 }
