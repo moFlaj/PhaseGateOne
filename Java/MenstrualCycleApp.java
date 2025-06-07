d3367e8f1633c013063e0aa1b9221911eddcf8a8
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-07 18:55:51 +0100

    Update MenstrualCycleApp.java
    Refactored my code to avoid repetition. For example, I defined a function to return day of week of a specific date, a function to throw errors if date input is of invalid format and lastly, a function to format date input and output.

--- Java/MenstrualCycleApp.java
+++ Java/MenstrualCycleApp.java
@@ -4,33 +4,48 @@ import java.time.format.DateTimeParseException;
 
 public class MenstrualCycleAppMod{
 
-	public static String periodFlowDays(String dateOfFirstFlow){
+	public static String returnDayOfWeek(String date){
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		try{
-			LocalDate date = LocalDate.parse(dateOfFirstFlow, formatter);
-// To make sure input is of the format specified is why the "formatter" object is part of the arguments passed.
+		LocalDate customDate = LocalDate.parse(date, formatter);
 
-			String dayOfWeekOfStartOfFlow = date.getDayOfWeek().toString();
+		String dayOfWeek = customDate.getDayOfWeek().toString();
 // Found out getDayOfWeek() returns an enum type.
 
-			String capitalizeFirstLetterOfStartDay = dayOfWeekOfStartOfFlow.substring(0,1).toUpperCase() + dayOfWeekOfStartOfFlow.substring(1).toLowerCase();
+		String capitalizeFirstLetterOfDay = dayOfWeek.substring(0,1).toUpperCase() + dayOfWeek.substring(1).toLowerCase();
 
-			int averageOfFlowDays = (3 + 7)/2;
-// Flow days in women lasts around 3 - 7 days.
+		return capitalizeFirstLetterOfDay;
+	}
 
-			LocalDate newDate = date.plusDays(averageOfFlowDays);
-			String dayOfWeekOfEndOfFlow = newDate.getDayOfWeek().toString();
-			String capitalizeFirstLetterOfEndDay = dayOfWeekOfEndOfFlow.substring(0,1).toUpperCase() + dayOfWeekOfEndOfFlow.substring(1).toLowerCase();
+	public static void throwErrorsIfDateInputIsInvalid(String date){
+//Created this function to avoid repeating the try and catch in all functions of this task. Called this function in the first line of the following functions so errors would be thrown if date input is invalid.
 
-			return "Your period will likely last from " + capitalizeFirstLetterOfStartDay + ", " + date + " to " + capitalizeFirstLetterOfEndDay + ", " + newDate.format(formatter) + ".";
-
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		try{
+			LocalDate customDate = LocalDate.parse(date, formatter);
 		}catch(DateTimeParseException invalidDate){
-
-//Throws error if input format is invalid. This can also be made more strict using the Pattern and Matcher classes.
-
 			throw new IllegalArgumentException("Invalid date format or values. Expected format: YYYY-MM-DD.");
 
 		}
+
 	}
 
+	public static LocalDate formatDate(String date){
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate customDate = LocalDate.parse(date, formatter);
+// To make sure input is of the format specified is why the "formatter" object is part of the arguments passed.
+
+		return customDate;
+	}
+
+	public static String periodFlowDays(String dateOfFirstFlow){
+
+		throwErrorsIfDateInputIsInvalid(dateOfFirstFlow);
+
+		int averageOfFlowDays = (3 + 7)/2;
+// Flow days in women lasts around 3 - 7 days.
+
+
+		return "Your period will likely last from " + returnDayOfWeek(dateOfFirstFlow) + ", " + formatDate(dateOfFirstFlow) + " to " + returnDayOfWeek((formatDate(dateOfFirstFlow).plusDays(averageOfFlowDays).toString())) + ", " + formatDate(dateOfFirstFlow).plusDays(averageOfFlowDays)  + ".";
+
+	}
 }
