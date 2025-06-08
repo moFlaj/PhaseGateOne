414b161a17464e9e15aad975250be65bd03a456a
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-08 14:21:35 +0100

    Add files via upload

---
+++ Python/test_menstrual_cycle_app.py
@@ -0,0 +1,22 @@
+import unittest
+from datetime import datetime, date, timedelta
+from menstrual_cycle_app import *
+
+
+class test_menstrual_cycle_app(unittest.TestCase):
+	
+
+	def setUp(self):
+		self.date_input = '2025/06/08'
+
+
+	def test_raise_errors_if_date_format_invalid_raises_error_if_entered_date_format_is_invalid(self):
+		date_input = '2025-06-08'
+		self.assertRaises(ValueError, raise_errors_if_format_invalid, date_input)
+
+	def test_return_day_of_week_returns_correct_day_of_date_input(self):
+
+		format_date_input = raise_errors_if_format_invalid(self.date_input)
+		actual = return_day_of_week(format_date_input)
+		expected = 'Sunday'
+		self.assertEqual(actual, expected)
