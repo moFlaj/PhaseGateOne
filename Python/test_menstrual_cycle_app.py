4af5544ebab2b6a1368d27549c7da4bee04a34bc
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-08 14:46:58 +0100

    Update test_menstrual_cycle_app.py

--- Python/test_menstrual_cycle_app.py
+++ Python/test_menstrual_cycle_app.py
@@ -9,17 +9,26 @@ class test_menstrual_cycle_app(unittest.TestCase):
 	def setUp(self):
 		self.date_input = '2025/06/08'
 
-
 	def test_raise_errors_if_date_format_invalid_raises_error_if_entered_date_format_is_invalid(self):
 		date_input = '2025-06-08'
-		self.assertRaises(ValueError, raise_errors_if_format_invalid, date_input)
+		self.assertRaises(ValueError, raise_errors_if_format_invalid_or_return_formatted_date, date_input)
 
 	def test_return_day_of_week_only_accepts_date_objects_as_arguments(self):
 		self.assertRaises(ValueError, return_day_of_week, self.date_input)
-
+		
 	def test_return_day_of_week_returns_correct_day_of_date_input(self):
 
-		format_date_input = raise_errors_if_format_invalid(self.date_input)
+		format_date_input = raise_errors_if_format_invalid_or_return_formatted_date(self.date_input)
 		actual = return_day_of_week(format_date_input)
 		expected = 'Sunday'
 		self.assertEqual(actual, expected)
+
+	def test_period_flow_days_returns_estimation_of_period_flow_days(self):
+		user_average_no_of_flow_days = 4
+		actual = period_flow_days(self.date_input, user_average_no_of_flow_days)
+		expected = 'Your period flow will last from Sunday, 08 June, 2025 to Thursday, 12 June, 2025.'
+		self.assertEqual(actual, expected)
+	
+	def test_period_flow_days_throws_error_if_second_argument_is_not_of_type_int(self):
+		user_average_no_of_flow_days = 4.5
+		self.assertRaises(ValueError, period_flow_days, self.date_input, user_average_no_of_flow_days)
