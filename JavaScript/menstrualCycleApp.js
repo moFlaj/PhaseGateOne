a84a5ddf834a40125ee0cee1266fd74b11da5f41
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-09 03:39:00 +0100

    Update menstrualCycleApp.js

--- JavaScript/menstrualCycleApp.js
+++ JavaScript/menstrualCycleApp.js
@@ -28,11 +28,24 @@ function periodFlowDays(dateInput, userAverageNoOfFlowDays){
 
 function findOvulationDay(nextPeriodStartDate){
 	let ovulationDay = 14	
+// Ovulation day occurs 14 days before start of next period.	
 	return 'Ovulation day of current period is on ' + returnDayOfWeek(nextPeriodStartDate.subtract(ovulationDay, 'day')) + ', ' + nextPeriodStartDate.subtract(ovulationDay, 'day').format("DD MMMM, YYYY") + '.'
 }
 
+function identifyFertileWindow(ovulationDay){
+	let fertileWindowBefore = throwErrorsIfDateInputInvalidOrReturnFormattedDate(ovulationDay).subtract(5, 'day')
+	let fertileWindowAfter = throwErrorsIfDateInputInvalidOrReturnFormattedDate(ovulationDay).add(1, 'day')
+// Fertile window begins five days before ovulation day and ends, one day after ovulation day.
+	return 'Fertile window will open on ' + returnDayOfWeek(fertileWindowBefore) + ', ' + fertileWindowBefore.format("DD MMMM, YYYY") + ' and closes on ' + returnDayOfWeek(fertileWindowAfter) + ', ' + fertileWindowAfter.format("DD MMMM, YYYY") + '.'
+
+}
 
 console.log(throwErrorsIfDateInputInvalidOrReturnFormattedDate("2025/06/09").format("YYYY/MM/DD"))
 let format_date = throwErrorsIfDateInputInvalidOrReturnFormattedDate("2025/06/09")
 console.log(returnDayOfWeek(format_date))
 console.log(periodFlowDays("2025/06/09", 4))
+console.log(nextPeriodStartDate("2025/06/09", 28))
+let nextPeriodDate = throwErrorsIfDateInputInvalidOrReturnFormattedDate("2025/06/09").add(28,'day')
+console.log(findOvulationDay(nextPeriodDate))
+let ovulationDay = throwErrorsIfDateInputInvalidOrReturnFormattedDate("2025/06/09").add(28,'day').subtract(14,'day')
+console.log(identifyFertileWindow(ovulationDay))
