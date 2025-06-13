2ebc2a35e66cd92112f38d6d6b6d8882f6860326
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-13 10:51:45 +0100

    Update TestCheckoutSystem.java

--- Java/TestCheckoutSystem.java
+++ Java/TestCheckoutSystem.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.InputMismatchException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class TestCheckoutSystem{
 
@@ -18,6 +19,7 @@ public class TestCheckoutSystem{
 	String actual;
 	String expected;
 	String product;
+	String searchInput;
 	String[] allProducts;
 	String[] allPrices;
 	String[] purchasedItemInfo;
@@ -29,12 +31,13 @@ public class TestCheckoutSystem{
 		lastName = "folorunsho";
 		middleName = "ayomide";
 		customerInfo = new String[1];
-		allProducts = new String[] {"Milk	50g", "Milk	25g", "Cornflakes	35g", "Cornflakes	15g", "Milo	20g", "Rice	50g", "Semolina	30g", "Frozen Chicken	20g", "Frozen chicken 	15g", "ChiActive	10g", "Ribena	8g"};
+		allProducts = new String[] { "ChiActive 10g", "Cornflakes 15g", "Cornflakes 35g", "Frozen chicken 15g", "Frozen Chicken 20g", "Milk 25g", "Milk 50g", "Milo 20g", "Ribena 8g", "Rice 50g", "Semolina 30g" };
 
 		allPrices = new String[] {"1700.00", "1200.00", "3000.00", "8000.00", "15700.00", "2200.00", "4200.00", "3500.00", "1100.00", "5450.00", "5800.00"};
 
 		purchasedItemInfo = new String[4];
 		customerPurchase = new ArrayList<>();
+		customerPurchase.add(new String[]{"ITEM", "QTY", "PRICE", "TOTAL(NGN)"});
 	}
 
 	@Test
@@ -79,37 +82,39 @@ public class TestCheckoutSystem{
 	@Test
 	void testGetCustomerNameReturnsFirstLettersOfNamesCapitalizedIfNamesAreValid(){
 		CheckoutSystem.setNameOfCustomer(firstName, lastName, middleName, customerInfo);
-		assertEquals("Adebola Folorunsho Ayomide", CheckoutSystem.getCustomerName(customerInfo));
+		assertEquals("Customer name: Adebola Folorunsho Ayomide", CheckoutSystem.getCustomerName(customerInfo));
 	}
+
+/*	@Test
+	void testReturnProductIfDetectedReturnsListOfItemsMatchingSearchInputForCashierToPick(){
+		searchInput = "mil";
+// If cashier wants to pick "Milk 25g" for example, and they enter "mil. Prompt for input in this function."
+		actual = CheckoutSystem.returnProductIfDetected(searchInput, allProducts);
+		expected = "Milk 25g";
+		assertEquals(expected, actual);
+	}
+*/ commented out because input is required
 
 	@Test
 	void testGetProductPriceReturnsPriceOfProductEnteredIfSizeIsIncluded(){
-		product = "milk	25g";
-//one tab spacing between product and size
+		product = "milk 25g";
 		assertEquals("NGN 2200.00", CheckoutSystem.getProductPrice(product, allProducts, allPrices, purchasedItemInfo));
 	}
 
 	@Test
-	void testGetProductPriceReturnsProductSizeAndPriceIfSearchInputIsNotCompletelyStated(){
-		product = "mil";
-		assertEquals("1. Milk	25g	2200.00\n2. Milk	50g	4200.00\n3. Milo	20g	3500.00\n", CheckoutSystem.getProductPrice(product, allProducts, allPrices, purchasedItemInfo));
-
-	}
-
-	@Test
 	void testAddPurchasedProductToListReturnsSuccessfulIfQuantityEnteredIsAValidInteger(){
-		product = "milk	25g";
+		product = "milk 25g";
 		CheckoutSystem.getProductPrice(product, allProducts, allPrices, purchasedItemInfo);
 		String quantity = "2";
 		actual = CheckoutSystem.addPurchasedProductToList(purchasedItemInfo, quantity, customerPurchase);
-		expected = "Product added succesfully. Add more items?";
+		expected = "Product added succesfully. Add more items?\nEnter Yes/No";
 		assertEquals(expected, actual);
 
 	}
 
 	@Test
 	void testAddPurchasedProductToListThrowsErrorIfQuantityEnteredIsNotAValidInteger(){
-		product = "milk	25g";
+		product = "milk 25g";
 		CheckoutSystem.getProductPrice(product, allProducts, allPrices, purchasedItemInfo);
 		String quantity = "2.5";
 		assertThrows(IllegalArgumentException.class, () -> {
@@ -118,13 +123,24 @@ public class TestCheckoutSystem{
 	}
 
 	@Test
+	void testAddPurchasedProductToListThrowsErrorIfQuantityEnteredIsLessThanOne(){
+		product = "milk 25g";
+		CheckoutSystem.getProductPrice(product, allProducts, allPrices, purchasedItemInfo);
+		String quantity = "0";
+		assertThrows(IllegalArgumentException.class, () -> {
+			CheckoutSystem.addPurchasedProductToList(purchasedItemInfo, quantity, customerPurchase);
+		});
+	}
+
+	@Test
 	void testComputeTotalCostOfItemsPurchased(){
-		product = "milk	25g";
+		
+		product = "milk 25g";
 		CheckoutSystem.getProductPrice(product, allProducts, allPrices, purchasedItemInfo);
 		String quantity = "2";
 		CheckoutSystem.addPurchasedProductToList(purchasedItemInfo, quantity, customerPurchase);
 		purchasedItemInfo = new String[4];
-		product = "cornflakes	35g";
+		product = "cornflakes 35g";
 		CheckoutSystem.getProductPrice(product, allProducts, allPrices, purchasedItemInfo);
 		quantity = "5";
 		CheckoutSystem.addPurchasedProductToList(purchasedItemInfo, quantity, customerPurchase);
