6242fe10400c3d41d1e53c2d72cb2a2c33c1e993
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-16 16:41:26 +0100

    Update phoneBook.py

--- Python/phoneBook.py
+++ Python/phoneBook.py
@@ -7,7 +7,7 @@ def add_contact(first_name, last_name, phone_number, contact_to_be_added, phone_
 		raise ValueError("Enter complete phone number.")
 
 	if first_name.strip() == '' or last_name.strip() == '':
-		raise ValueError("This is a required field. Cannot be left blank.")
+		raise ValueError("These are all required fields. Cannot be left blank.")
 
 	if not first_name.isalpha() or not last_name.isalpha():
 		raise ValueError("Name field may contain invalid characters.")
@@ -26,13 +26,15 @@ def remove_contact(name, phone_book):
 	return phone_book
 
 
-def find_contact_by_phone_number(phone_number, phone_book):
-	check_phone_number = Decimal(phone_number)
+def find_contact_by_phone_number(number, phone_book):
+
+	check_phone_number = Decimal(number)
 	found_contact = []
 
 	for each_contact in phone_book:
-		if phone_number in each_contact:
-			found_contact.append(each_contact)
+		for phone_number in each_contact:
+			if number in phone_number:
+				found_contact.append(each_contact)
 
 	if len(found_contact) == 0:
 		raise ValueError("No contacts found.")
@@ -55,7 +57,6 @@ def find_contact_by_first_name(first_name, phone_book):
 
 def find_contact_by_last_name(last_name, phone_book):
 	found_contacts = []
-	if_found = ""
 
 	for each_contact in phone_book:
 		if last_name.lower() in each_contact[0].lower():
@@ -69,25 +70,28 @@ def find_contact_by_last_name(last_name, phone_book):
 
 
 def edit_contact(full_name, phone_book):
+
 	for each_contact in phone_book:
+
 		if each_contact[0].lower() == full_name.lower():
 			print("Which field do you want to edit.\nEnter 1 for name or 2 for phone number")
 			select = int(input())
 			if select == 1:
-				each_contact[0] = input("Enter new name: ")
+				each_contact[0] = input("Edit name: ")
 			elif select == 2:
- 				each_contact[1] = input("Enter new phone number: ")
+ 				each_contact[1] = input("Edit phone number: ")
 
 	return phone_book
 
 
-firstName = ""
-lastName = ""
-phoneNumber = ""
-fullName = ""
-phoneBook = []
-phoneBookRunner = True
-phoneBookMenu = """
+
+first_name = ""
+last_name = ""
+phone_number = ""
+full_name = ""
+phone_book = []
+phone_book_runner = True
+phone_book_menu = """
 1. Add contact
 2. Remove contact
 3. Find contact by phone number
@@ -97,112 +101,152 @@ phoneBookMenu = """
 7. Exit
 """
 
-while phoneBookRunner:
-	print(phoneBookMenu)
+while phone_book_runner:
+	print(phone_book_menu)
 	select = input()
 
 	match select:
 		case "1":
-			addContact = True
-			while addContact:
-				contactToBeAdded = ["", ""]
-				firstName = "Adebola"
-				lastName = "Foley"
-				phoneNumber = "08103195663"
-				phoneBook = addContact(firstName, lastName, phoneNumber, contactToBeAdded, phoneBook)
-				backTo = True
-				while backTo:
-					print("Enter 0 to return")
-					backToLastPage = input()
-					match backToLastPage:
-						case "0":
-							addContact = False
-							backTo = False
-						case _:
-							print("Invalid input.")
+			add = True
+			while add:
+				try:
+					contact_to_be_added = ["", ""]
+					first_name = input('First name: ')
+					last_name = input('Last name: ')
+					phone_number = input('Phone number: ')
+					phone_book_size_after = len(phone_book) + 1
+					phone_book = add_contact(first_name, last_name, phone_number, contact_to_be_added, phone_book)
+
+				except ValueError as invalid_input:
+					print(invalid_input.args[0]);
+				if len(phone_book) ==  phone_book_size_after:	
+					back_to = True
+					while back_to:
+						print("Enter 0 to return")
+						back_to_last_page = input()
+						match back_to_last_page:
+							case "0":
+								add = False
+								back_to = False
+							case _:
+								print("Invalid input.")
+				else:
+					continue
 
 		case "2":
-			removeCon = True
-			while removeCon:
-				fullName = "Adebola Foley"
-				phoneBook = removeContact(fullName, phoneBook)
-				backTo = True
-				while backTo:
-					print("Enter 0 to return")
-					backToLastPage = input()
-					match backToLastPage:
-						case "0":
-							removeCon = False
-							backTo = False
-						case _:
-							print("Invalid input.")
+			try:
+				full_name = input('Full name of contact: ')
+				phone_book = remove_contact(full_name, phone_book)
+
+			except ValueError as emptyPhoneBook:
+				print(emptyPhoneBook.args[0])
+			back_to = True
+			while back_to:
+				print("Enter 0 to return")
+				back_to_last_page = input()
+				match back_to_last_page:
+					case "0":
+						back_to = False
+					case _:
+						print("Invalid input.")
+
+		
 
 		case "3":
-			findContactByNumber = True
-			while findContactByNumber:
-				phoneNumber = "08103195663"
-				print(findContactByPhoneNumber(phoneNumber, phoneBook))
-				backTo = True
-				while backTo:
-					print("Enter 0 to return")
-					backToLastPage = input()
-					match backToLastPage:
+			find_contact_by_number = True
+			while find_contact_by_number:
+				try:
+					phone_number = input('Enter phone number: ')
+					print(find_contact_by_phone_number(phone_number, phone_book))
+
+				except ValueError as no_contacts_found:
+					print(no_contacts_found.args[0])
+
+				back_to = True
+				while back_to:
+					print("Enter 0 to return or 1 to search again")
+					back_to_last_page = input()
+					match back_to_last_page:
 						case "0":
-							findContactByNumber = False
-							backTo = False
+							find_contact_by_number = False
+							back_to = False
+						case "1":
+							back_to = False
 						case _:
-							print("Invalid input.")
+ 							print("Invalid input.")
 
 		case "4":
-			findContactByFirst = True
-			while findContactByFirst:
-				firstName = "Adebola"
-				print(findContactByFirstName(firstName, phoneBook))
-				backTo = True
-				while backTo:
-					print("Enter 0 to return")
-					backToLastPage = input()
-					match backToLastPage:
+			
+			find_contact_by_first = True
+			while find_contact_by_first:
+				try:
+					first_name = input('Enter first name: ')
+					print(find_contact_by_first_name(first_name, phone_book))
+				except ValueError as no_contacts_found:
+					print(no_contacts_found.args[0])
+				back_to = True
+				while back_to:
+					print("Enter 0 to return or 1 to search again")
+					back_to_last_page = input()
+					match back_to_last_page:
 						case "0":
-							findContactByFirst = False
-							backTo = False
+							find_contact_by_first = False
+							back_to = False
+						case "1":
+							back_to = False
 						case _:
  							print("Invalid input.")
 
 		case "5":
-			findContactByLast = True
-			while findContactByLast:
-				lastName = "Foley"
-				print(findContactByLastName(lastName, phoneBook))
-				backTo = True
-				while backTo:
-					print("Enter 0 to return")
-					backToLastPage = input()
-					match backToLastPage:
+
+			find_contact_by_last = True
+
+			while find_contact_by_last:
+
+				try:
+					last_name = input('Enter last name: ')
+
+					print(find_contact_by_last_name(last_name, phone_book))
+
+				except ValueError as no_contacts_found:
+
+					print(no_contacts_found.args[0])
+
+				back_to = True
+				while back_to:
+
+					print("Enter 0 to return or 1 to search again")
+
+					back_to_last_page = input()
+
+					match back_to_last_page:
+
 						case "0":
-							findContactByLast = False
-							backTo = False
+							find_contact_by_last = False
+							back_to = False
+						case "1":
+							back_to = False
 						case _:
-							print("Invalid input.")
+ 							print("Invalid input.")
 
 		case "6":
-			editCon = True
-			while editCon:
-				fullName = "Adebola Foley"
-				phoneBook = editContact(fullName, phoneBook)
-				backTo = True
-				while backTo:
+			edit_con = True
+			while edit_con:
+				full_name = input('Enter full name of contact: ')
+				phone_book = edit_contact(full_name, phone_book)
+				back_to = True
+				while back_to:
 					print("Enter 0 to return")
-					backToLastPage = input()
-					match backToLastPage:
+					back_to_last_page = input()
+					match back_to_last_page:
 						case "0":
-							editCon = False
-							backTo = False
+							edit_con = False
+							back_to = False
 						case _:
  							print("Invalid input.")
 
 		case "7":
-			phoneBookRunner = False
+			phone_book_runner = False
 
 		case _:
 			print("Invalid input.")
