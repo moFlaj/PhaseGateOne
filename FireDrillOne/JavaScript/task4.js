9324af0ee2a7e19525bfd9bc4fe81b1174c9a56a
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-17 17:24:25 +0100

    Create task4.js

---
+++ FireDrillOne/JavaScript/task4.js
@@ -0,0 +1,19 @@
+//Task 1
+for(let counter = 1; counter <= 10; counter++){
+	console.log(counter);
+} 
+
+//Task 2
+for(let counter = 2; counter <= 10; counter+=2){
+	console.log(counter);
+} 
+
+//Task 3
+for(let counter = 1; counter <= 10; counter+=2){
+	console.log(counter);
+} 
+
+//Task 4
+for(let counter = 4; counter <= 10; counter+=4){
+	console.log(counter);
+} 
