b3e269d9933e45bf2fd8b3d9383ca84bc7070a9e
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-17 17:28:05 +0100

    Create task4.py

---
+++ FireDrillOne/Python/task4.py
@@ -0,0 +1,15 @@
+#Task 1
+for counter in range(1, 11):
+	print(counter)
+
+#Task 2
+for counter in range(2, 11, 2):
+	print(counter)
+
+#Task 3
+for counter in range(1, 11, 2):
+	print(counter)
+
+#Task 4
+for counter in range(4, 11, 4):
+	print(counter)
