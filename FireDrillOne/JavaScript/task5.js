c66cdcad5b5630ec2742002c610d58d5968604d5
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-17 17:25:12 +0100

    Create task5.js

---
+++ FireDrillOne/JavaScript/task5.js
@@ -0,0 +1,27 @@
+//Task 1
+for(let counter = 1; counter <= 10; counter++){
+	console.log(counter);
+} 
+
+//Task 2
+for(let counter = 2; counter <= 10; counter+=2){
+	console.log(counter);
+} 
+
+//Task 3
+for(let counter = 1; counter <= 10; counter+=2){
+	console.log(counter);
+} 
+
+//Task 4
+for(let counter = 4; counter <= 10; counter+=4){
+	console.log(counter);
+} 
+
+//Task 5
+for(let counter = 4; counter <= 10; counter+=4){
+	for(let printResultFiveTimes = 0; printResultFiveTimes < 5; printResultFiveTimes++){
+		process.stdout.write("" + counter);
+	}
+	process.stdout.write(" ");
+}
