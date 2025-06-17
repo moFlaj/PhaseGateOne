8858bf99d8c1b8dfff3c5ac47751d55b043b2283
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-17 17:30:08 +0100

    Create Task4.java

---
+++ FireDrillOne/Java/Task4.java
@@ -0,0 +1,23 @@
+public class Task4{
+
+	public static void main(String[] args){
+
+//Task 1
+		for(int counter = 1; counter <= 10; counter++){
+			System.out.println(counter);
+		}
+
+//Task 2
+		for(int counter = 2; counter <= 10; counter+=2){
+			System.out.println(counter);
+		}
+
+//Task 3
+		for(int counter = 1; counter <= 10; counter+=2){
+			System.out.println(counter);
+		}
+
+//Task 4
+		for(int counter = 4; counter <= 10; counter+=4){
+			System.out.println(counter);
+		}
