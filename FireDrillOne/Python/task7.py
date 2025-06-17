e2815ba8389404070d1119e39aa0a87edb59abef
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-17 17:29:32 +0100

    Create task7.py

---
+++ FireDrillOne/Python/task7.py
@@ -0,0 +1,38 @@
+#Task 1
+for counter in range(1, 11):
+	print(counter)
+
+#Task 2
+for counter in range(2, 11, 2):
+	print(counter)
+
+#Task 3
+for counter in range(1, 11, 2):
+	print(counter)
+
+#Task 4
+for counter in range(4, 11, 4):
+	print(counter)
+
+#Task 5
+for counter in range(4, 11, 4):
+	for print_result_five_times in range(5):
+		print(counter, end = "")
+	print(end = ' ')
+
+print()
+
+#Task 6
+for counter in range(4, 11, 4):
+	for multiples in range(1,6):
+		print(pow(counter,multiples) , end = " ")
+	
+print()
+
+#Task 7
+for counter in range(4, 11, 4):
+	sum = 0
+	for multiples in range(1,6):
+		sum += pow(counter,multiples)
+	print(sum, end = " ")	
+print()
