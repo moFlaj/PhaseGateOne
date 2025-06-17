1fed32de92c0afdc5853a29c189f560d6a1ef698
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-17 16:29:40 +0100

    Update TaskTwo.java

--- FireDrillOne/Java/TaskTwo.java
+++ FireDrillOne/Java/TaskTwo.java
@@ -1,1 +1,16 @@
+public class TaskTwo{
 
+	public static void main(String[] args){
+
+//Task 1
+		for(int counter = 1; counter <= 10; counter++){
+			System.out.println(counter);
+		}
+//Task 2
+		for(int counter = 2; counter <= 10; counter+=2){
+			System.out.println(counter);
+		}
+
+	}
+
+}
