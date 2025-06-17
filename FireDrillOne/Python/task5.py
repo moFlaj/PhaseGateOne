da80d9e84d13cf90fd19c50ea2c1d17a6f5f2f46
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-17 17:28:45 +0100

    Create task5.py

---
+++ FireDrillOne/Python/task5.py
@@ -0,0 +1,23 @@
+#Task 1
+for counter in range(1, 11):
+	print(counter)
+
+#Task 2
+for counter in range(2, 11, 2):
+	print(counter)
+
+#Task 3
+for counter in range(1, 11, 2):
+	print(counter)
+
+#Task 4
+for counter in range(4, 11, 4):
+	print(counter)
+
+#Task 5
+for counter in range(4, 11, 4):
+	for print_result_five_times in range(5):
+		print(counter, end = "")
+	print(end = ' ')
+
+print()
