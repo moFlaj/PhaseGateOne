b1ce31926aba72f889f1ad25a6aed149cb62c3c8
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-17 16:37:14 +0100

    Create TaskThree.java

---
+++ FireDrillOne/Java/TaskThree.java
@@ -0,0 +1,22 @@
+public class TaskOne{
+
+	public static void main(String[] args){
+
+//Task 1
+		for(int counter = 1; counter <= 10; counter++){
+			System.out.println(counter);
+		}
+
+//Task 2
+		for(int counter = 2; counter <= 10; counter+=2){
+			System.out.println(counter);
+		}
+
+//Task 3
+		for(int counter = 1; counter <= 10; counter+=2){
+			System.out.println(counter);
+    }
+
+	}
+
+}
