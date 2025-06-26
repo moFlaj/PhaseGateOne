7e9c3822d08d0cd50a2c01f3f92535cacd9d8718
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-06-26 20:33:06 +0100

    Update StudentGrade.java

--- Java/StudentGrade.java
+++ Java/StudentGrade.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 public class StudentGrade{
 
@@ -13,19 +14,24 @@ public class StudentGrade{
 
 		for(double[] eachStudent : allStudents){
 			double total = 0;
+			String score = "";
 			for(int subjectScore = 0; subjectScore < noOfSubjects; subjectScore++){
 				while(true){
-					System.out.println("Entering score for student " + studentCounter);
-					System.out.println("Enter score for subject " + (subjectScore + 1));
-					double score = input.nextDouble();
-
-					if(100 >= score && score >= 0){
-						total+=score;
-						eachStudent[subjectScore] = score;
-						break;
-					}
-					else{
-						System.out.println("Invalid score.\nScore must be between 0 and 100!");
+					try{
+						System.out.println("Entering score for student " + studentCounter);
+						System.out.println("Enter score for subject " + (subjectScore + 1));
+						score = input.next();
+						double convertScore = Double.parseDouble(score);
+						if(100 >= convertScore && convertScore >= 0){
+							total+=convertScore;
+							eachStudent[subjectScore] = convertScore;
+							break;
+						}
+						else{
+							System.out.println("Invalid score.\nScore must be between 0 and 100!");
+						}
+					}catch(NumberFormatException invalidInput){
+						System.out.println("You may have entered an invalid character. Retry.");
 					}
 				}
 			}	
@@ -43,7 +49,7 @@ public class StudentGrade{
 
 		for(double[] eachStudent : allStudents){
 
-			averageScoresOfEachStudent.add(eachStudent[eachStudent.length - 2]); //adds each studen average score to a list.
+			averageScoresOfEachStudent.add(eachStudent[eachStudent.length - 2]); //adds each student average score to a list.
 		}
 
 		Collections.sort(averageScoresOfEachStudent, Collections.reverseOrder()); //To sort the all students average score in descending order.
@@ -123,7 +129,7 @@ public class StudentGrade{
 					highest = eachStudentScoreInSubject[eachSubjectIndex];
 					highestScoringStudent = indexOfHighest + 1;
 				}
-				else if(eachStudentScoreInSubject[eachSubjectIndex] < lowest){
+				if(eachStudentScoreInSubject[eachSubjectIndex] < lowest){
 					lowest = eachStudentScoreInSubject[eachSubjectIndex];
 					lowestScoringStudent = indexOfLowest + 1;
 				}
@@ -137,8 +143,8 @@ public class StudentGrade{
 			}
 			
 			System.out.printf("Subject %d%n", (eachSubjectIndex + 1));
-			System.out.printf("Highest scoring student is: Student %d scoring %2f.%n", highestScoringStudent, highest);
-			System.out.printf("Lowest scoring student is: Student %d scoring %2f.%n", lowestScoringStudent, lowest);
+			System.out.printf("Highest scoring student is: Student %d scoring %.2f.%n", highestScoringStudent, highest);
+			System.out.printf("Lowest scoring student is: Student %d scoring %.2f.%n", lowestScoringStudent, lowest);
 			System.out.printf("Total score is: %.2f.%n", sumSubjectTotal);
 			System.out.printf("Average score is: %.2f.%n", (sumSubjectTotal/noOfStudents));
 			System.out.printf("Number of passes: %d.%n", countNoOfPasses);
@@ -149,18 +155,153 @@ public class StudentGrade{
 
 	}
 
+	public static void classSummary(double[][] allStudents, int noOfSubjects, int noOfStudents){
+		
+		int compareSubjectFailures = 0;
+		int compareSubjectPasses = 0;
+		double overallHighestSubjectScore = Integer.MIN_VALUE;
+		double overallLowestestSubjectScore = Integer.MAX_VALUE;
+		int allStudentsIndex = 0;
+		int highestScoreInAllIndex = 0;
+		int subjectWithHighestScore = 0;
+		int lowestScoreInAll = 0;
+		int subjectWithLowestScore = 0;
+		double sumStudentTotal = 0;
+		double highestStudentsTotal = Integer.MIN_VALUE;
+		double lowestStudentsTotal = Integer.MAX_VALUE;
+		int bestGraduatingStudent = 0;
+		int worstGraduatingStudent = 0;
+		int hardestSubjectIndex = 0;
+		int easiestSubjectIndex = 0;
+		double classTotalScore = 0;
+		int countNoOfPasses = 0;
+		int countNoOfFailures = 0;
+
+		for(int eachSubjectIndex = 0; eachSubjectIndex < noOfSubjects; eachSubjectIndex++){
+
+			for(double[] eachStudentScoreInSubject : allStudents){
+
+				if(eachStudentScoreInSubject[eachSubjectIndex] > overallHighestSubjectScore){
+					overallHighestSubjectScore = eachStudentScoreInSubject[eachSubjectIndex];
+					highestScoreInAllIndex = allStudentsIndex + 1;
+					subjectWithHighestScore = eachSubjectIndex + 1;
+				}
+				
+				if(eachStudentScoreInSubject[eachSubjectIndex] < overallLowestestSubjectScore){
+					overallLowestestSubjectScore = eachStudentScoreInSubject[eachSubjectIndex];
+					lowestScoreInAll = allStudentsIndex + 1;
+					subjectWithLowestScore = eachSubjectIndex + 1;	
+				}
+
+				if(eachStudentScoreInSubject[eachSubjectIndex] >= 50)countNoOfPasses++;
+
+				else{countNoOfFailures++;}
+
+				if(eachSubjectIndex == 0){
+					for(int addScoresOfStudent = 0; addScoresOfStudent < noOfSubjects; addScoresOfStudent++){
+						sumStudentTotal += eachStudentScoreInSubject[addScoresOfStudent];
+					}
+					classTotalScore += sumStudentTotal;
+
+					if(sumStudentTotal > highestStudentsTotal){
+						highestStudentsTotal = sumStudentTotal;
+						bestGraduatingStudent = allStudentsIndex + 1;	
+					}
+					if(sumStudentTotal < lowestStudentsTotal){
+						lowestStudentsTotal = sumStudentTotal;
+						worstGraduatingStudent = allStudentsIndex + 1;	
+					}
+				}
+				allStudentsIndex++;
+				sumStudentTotal = 0;
+			}
+			if(eachSubjectIndex == 0){
+				compareSubjectFailures = countNoOfFailures;
+				compareSubjectPasses = compareSubjectPasses;
+				hardestSubjectIndex = eachSubjectIndex + 1;
+				easiestSubjectIndex = eachSubjectIndex + 1;
+			}
+			else{
+				if(countNoOfPasses > compareSubjectPasses){
+					compareSubjectPasses = countNoOfPasses;
+					easiestSubjectIndex = eachSubjectIndex + 1;
+				}
+				else if(countNoOfFailures > compareSubjectFailures){
+					compareSubjectFailures = countNoOfFailures;
+					hardestSubjectIndex = eachSubjectIndex + 1;
+				}
+			}
+			countNoOfFailures = 0;
+			countNoOfPasses = 0;
+			allStudentsIndex = 0;
+		}
+		if(compareSubjectPasses > 0 && compareSubjectFailures > 0){
+			if(compareSubjectFailures > compareSubjectPasses || compareSubjectFailures < compareSubjectPasses){
+				System.out.printf("%n%nThe hardest subject is subject %d with %d failures.%n", hardestSubjectIndex, compareSubjectFailures);
+				System.out.printf("The easiest subject is subject %d with %d passes.%n", easiestSubjectIndex, compareSubjectPasses);	
+				System.out.printf("The overall highest score is scored by student %d in subject %d scoring %.2f.%n", highestScoreInAllIndex, subjectWithHighestScore, overallHighestSubjectScore);
+				System.out.printf("The overall lowest score is scored by student %d in subject %d scoring %.2f.%n", lowestScoreInAll, subjectWithLowestScore, overallLowestestSubjectScore);
+			}else{
+				System.out.println("There were equal number of passes and failures in all subjects.");
+				System.out.printf("The overall highest score is scored by student %d in subject %d scoring %.2f.%n", highestScoreInAllIndex, subjectWithHighestScore, overallHighestSubjectScore);
+				System.out.printf("The overall lowest score is scored by student %d in subject %d scoring %.2f.%n", lowestScoreInAll, subjectWithLowestScore, overallLowestestSubjectScore);
+			}
+			System.out.printf("%n%n%nCLASS SUMMARY%n%n");
+			System.out.printf("Best graduating student is: student %d scoring %.2f.%n", bestGraduatingStudent, highestStudentsTotal);
+			System.out.printf("Worst graduating student is: student %d scoring %.2f.%n%n%n", worstGraduatingStudent, lowestStudentsTotal);
+
+		}else if(compareSubjectPasses > 0 && compareSubjectFailures == 0){
+			System.out.println("No failures");
+		}else if(compareSubjectPasses == 0 && compareSubjectFailures > 0){
+			System.out.println("All students failed all subjects");
+		}
+		
+		System.out.printf("Class total score is %.2f.%n", classTotalScore);
+		System.out.printf("Class average score is %.2f.%n", (classTotalScore/noOfStudents));
+
+	}
 
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		System.out.println("How many students do you have?");
-		int noOfStudents = input.nextInt();
-		System.out.println("How many subjects do they offer?");
-		int noOfSubjects = input.nextInt();
-		double[][] allStudents = {{}};
-		allStudents = inputStudentScores(allStudents, noOfStudents, noOfSubjects);
-		allStudents =  postionOfStudents(allStudents);
-		displayRecordSheet(allStudents, noOfSubjects);
-		subjectSummary(allStudents, noOfSubjects, noOfStudents);
+		String noOfStudents = "";
+		String noOfSubjects = "";
+		int convertStu = 0;
+		int convertSub = 0;
+		boolean runApp = true;
+
+			while(runApp){
+				try{
+					System.out.println("How many students do you have?");
+					noOfStudents = input.next();
+					convertStu = Integer.parseInt(noOfStudents);
+					if(convertStu < 1)throw new IllegalArgumentException("This field requires a positive integer.");
+						
+					
+					System.out.println("How many subjects do they offer?");
+					noOfSubjects = input.next();
+					convertSub = Integer.parseInt(noOfSubjects);
+					if(convertSub < 1)throw new IllegalArgumentException("This field requires a positive integer. Retry.");
+					
+
+					double[][] allStudents = {{}};
+					allStudents = inputStudentScores(allStudents, convertStu, convertSub);
+					allStudents =  postionOfStudents(allStudents);
+					displayRecordSheet(allStudents, convertSub);
+					subjectSummary(allStudents, convertSub, convertStu);
+					classSummary(allStudents, convertSub, convertStu);
+					runApp = false;
+				
+
+				}catch(NumberFormatException invalidInput){
+					System.out.println("You may have entered an invalid character. Start over");
+				}
+				catch(IllegalArgumentException invalid){
+					System.out.println(invalid.getMessage());
+				}
+			}
+			
+				
+			
 		
 	}
 
